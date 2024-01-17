e3175503492abfe9107c7499502822b2e9f1e88d
Tom Sanchis <tomsanc@icloud.com>
2024-01-17 12:25:47 +0100

    feat(ajout des conditions pour changer la couleur du h2 et le surligner): ajout conditions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
-console.log("Hello World !");
-
 const AllLi = document.querySelectorAll("nav ul li a");
 const AllH2 = document.querySelectorAll("h2");
 
 for (let i = 0; i < AllLi.length; i++) {
   const element = AllLi[i];
+
+  element.addEventListener("click", () => {
+    for (let index = 0; index < AllH2.length; index++) {}
+    if (element.textContent === "Type de nouilles") {
+    } else if (element.textContent === "Définition") {
+    } else if (element.textContent === "Histoire") {
+    } else if (element.textContent === "Préparation") {
+    } else {
+      console.log("Error: " + element.textContent);
+    }
+  });
 }
