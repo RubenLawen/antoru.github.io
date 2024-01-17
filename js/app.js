65023844125d8a0b7374e24b97bdcb222670fd60
Tom Sanchis <tomsanc@icloud.com>
2024-01-17 11:25:41 +0100

    Add events clicks to all li form nav

--- js/app.js
+++ js/app.js
@@ -5,4 +5,8 @@ const AllH2 = document.querySelectorAll("h2");
 
 for (let i = 0; i < AllLi.length; i++) {
   const element = AllLi[i];
+  // Add events clicks to all li form nav
+  element.addEventListener("click", () => {
+    console.log("click");
+  });
 }
