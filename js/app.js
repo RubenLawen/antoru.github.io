2492de222d33235cd1cac35e279c284d0539867e
Tom Sanchis <tomsanc@icloud.com>
2024-01-17 11:14:25 +0100

    add for loop javascript pn li

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@ console.log("Hello World !");
 
 const AllLi = document.querySelectorAll("nav ul li a");
 const AllH2 = document.querySelectorAll("h2");
+
+for (let i = 0; i < AllLi.length; i++) {
+  const element = AllLi[i];
+}
